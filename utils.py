372cd37888d6c271c394d3275b79726c90286a02
Mac Wang <macwang1@gmail.com>
2017-03-04 22:07:19 +0800

    add movie converter
    add text

--- utils.py
+++ utils.py
@@ -55,8 +55,9 @@ def get_perspective_transform():
     dst = np.float32([[mid-tw, t2], [mid+tw, t2], [mid-tw, b2], [mid+tw, b2]])
 
     M = cv2.getPerspectiveTransform(src, dst)
+    Minv = cv2.getPerspectiveTransform(dst, src)
 
-    return M
+    return M, Minv
 
 def thresh_filter(s, thresh=(0, 255)):
     binary_output = np.zeros_like(s)
@@ -110,3 +111,28 @@ def combine_binaries(img1, img2):
     combined_binary = np.zeros_like(img1)
     combined_binary[(img1 == 1) | (img2 == 1)] = 1
     return combined_binary
+
+def draw_result(undist, warped, left_fit, right_fit, Minv):
+    # Create an image to draw the lines on
+    warp_zero = np.zeros_like(warped).astype(np.uint8)
+    color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+
+    ploty = np.linspace(0, warped.shape[0]-1, warped.shape[0])
+    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+    right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+
+    # Recast the x and y points into usable format for cv2.fillPoly()
+    pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+    pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+    pts = np.hstack((pts_left, pts_right))
+
+    # Draw the lane onto the warped blank image
+    cv2.fillPoly(color_warp, np.int_([pts]), (0,255, 0))
+
+    # Warp the blank back to original image space using inverse perspective matrix (Minv)
+    newwarp = cv2.warpPerspective(color_warp, Minv, (undist.shape[1], undist.shape[0]))
+    # Combine the result with the original image
+    result = cv2.addWeighted(undist, 1, newwarp, 0.3, 0)
+
+    return result
+    # plt.imshow(result)
